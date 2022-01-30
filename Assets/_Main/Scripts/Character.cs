a9ff9f0a12ef89569e3152935f043bef35ae9a61
LucasGaspar <lucas_gaspar@hotmail.com>
2022-01-30 13:33:33 -0600

    Improved movement

--- Assets/_Main/Scripts/Character.cs
+++ Assets/_Main/Scripts/Character.cs
@@ -1,22 +1,32 @@
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
 public class Character : MonoBehaviour
 {
-    private const float RunSpeed = 3f;
+    private const float RunSpeed = 2f;
     private const float JumpForce = 7.5f;
     private const float Gravity = -15f;
-    private const float GroundedGravity = 0f;
+    private const float GroundedGravity = -1f;
     private const float MaxFallSpeed = -10f;
+    private const string GroundLayerName = "Ground";
+    private const float RaycastGroundedLength = 0.10f;
+    private const float RaycastCeilingLength = 0.10f;
+    private const float RaycastOffsetFromBorder = 0.1f;
 
     private new Rigidbody2D rigidbody2D = null;
+    private new Collider2D collider2D = null;
     private float horizontalSpeed = 0f;
     private float verticalSpeed = 0f;
     private bool grounded = true;
+    private int groundLayer = 0;
+
+    private bool Rising { get => verticalSpeed > 0.25f; }
 
     private void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        collider2D = GetComponent<Collider2D>();
+        groundLayer = 1 << LayerMask.NameToLayer(GroundLayerName);
     }
 
     public void MoveLeft()
@@ -36,27 +46,66 @@ public class Character : MonoBehaviour
 
     public void Jump()
     {
+        if (!grounded)
+            return;
+
         verticalSpeed = JumpForce;
+        grounded = false;
     }
 
     private void FixedUpdate()
     {
-        grounded = transform.position.y <= 0 && verticalSpeed <= 0;
-        Vector2 newVelocity = new Vector2();
+        CalculateGrounded();
+        CalculateCeilingHit();
         if (grounded)
         {
             verticalSpeed = GroundedGravity;
-            newVelocity.y = verticalSpeed;
-            newVelocity.x = horizontalSpeed;
         }
         else
         {
             verticalSpeed += Gravity * Time.fixedDeltaTime;
             verticalSpeed = Mathf.Max(verticalSpeed, MaxFallSpeed);
-            newVelocity.y = verticalSpeed;
-            newVelocity.x = rigidbody2D.velocity.x;
         }
 
+        Vector2 newVelocity = new Vector2(horizontalSpeed, verticalSpeed);
         rigidbody2D.velocity = newVelocity;
+    }
+
+    private void CalculateGrounded()
+    {
+        if (Rising)
+        {
+            grounded = false;
+        }
+        else
+        {
+            //Test hit on lowest right
+            RaycastHit2D hit = Physics2D.Raycast(new Vector2(collider2D.bounds.max.x - RaycastOffsetFromBorder, collider2D.bounds.min.y), Vector2.down, RaycastGroundedLength, groundLayer);
+            grounded = (hit.collider != null);
+            if (!grounded)
+            {
+                //Test hit on lowest left
+                hit = Physics2D.Raycast(new Vector2(collider2D.bounds.min.x + RaycastOffsetFromBorder, collider2D.bounds.min.y), Vector2.down, RaycastGroundedLength, groundLayer);
+                grounded = (hit.collider != null);
+            }
+        }
+    }
+
+    private void CalculateCeilingHit()
+    {
+        if (grounded)
+            return;
+
+        //Test hit on upper right
+        RaycastHit2D hit = Physics2D.Raycast(new Vector2(collider2D.bounds.max.x - RaycastOffsetFromBorder, collider2D.bounds.max.y), Vector2.up, RaycastCeilingLength, groundLayer);
+        if (hit.collider == null)
+        {
+            //Test hit on upper left
+            hit = Physics2D.Raycast(new Vector2(collider2D.bounds.min.x + RaycastOffsetFromBorder, collider2D.bounds.max.y), Vector2.up, RaycastCeilingLength, groundLayer);
+            if (hit.collider == null)
+                return;
+        }
+
+        verticalSpeed = 0f;
     }
 }
